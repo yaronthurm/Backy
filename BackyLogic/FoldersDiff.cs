50389efbec2ba8681ba1c567d481e53615220a3e
yaronthurm <yaronthurm@gmail.com>
2016-02-29 13:47:59 +0200

    added class to do diff between folders

---
+++ BackyLogic/FoldersDiff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackyLogic
+{
+    public class FoldersDiff
+    {
+        public IEnumerable<BackyFile> NewFiles;
+        public IEnumerable<BackyFile> ModifiedFiles;
+        public IEnumerable<string> DeletedFiles;
+        public IEnumerable<RenameInfo> RenamedFiles;
+    }
+}
