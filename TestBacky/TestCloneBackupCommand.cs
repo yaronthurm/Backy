b504a5613819c8a1a14611c31cb3d1451e96b76b
yaronthurm <yaronthurm@gmail.com>
2016-03-24 20:02:59 +0200

    added TestCloneBackupCommand

---
+++ TestBacky/TestCloneBackupCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BackyLogic;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Security.AccessControl;
+
+namespace TestBacky
+{
+    [TestClass]
+    public class TestCloneBackupCommand
+    {
+        [TestMethod]
+        public void Test01_Playing_around()
+        {
+        }
+
+
+        private static void AssertLists<T>(IEnumerable<T> expected, IEnumerable<T> actual)
+        {
+            var diff = expected.Except(actual);
+            Assert.IsFalse(diff.Any(), "missing items");
+
+            diff = actual.Except(expected);
+            Assert.IsFalse(diff.Any(), "extra items");
+        }
+    }
+}
