46f11153ff1a4360e6041940fb127d5aba32680d
yaronthurm <yaronthurm@gmail.com>
2016-03-24 20:01:28 +0200

    added new command: CloneBackupCommand

---
+++ BackyLogic/CloneBackupCommand.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace BackyLogic
+{
+    public class CloneBackupCommand
+    {
+        private string _source;
+        private string _target;
+        private IFileSystem _fileSystem;
+        private bool _abort;
+        public IMultiStepProgress Progress;
+
+        public CloneBackupCommand(IFileSystem fileSystem, string source, string target)
+        {
+            _fileSystem = fileSystem;
+            _source = source;
+            _target = target;
+        }
+
+
+        public void Execute()
+        {
+            var sw = Stopwatch.StartNew();
+            try
+            {
+                this.Progress?.StartStepWithoutProgress("Started: " + DateTime.Now);
+            }
+            finally
+            {
+                sw.Stop();
+                this.Progress?.StartStepWithoutProgress("Finished: " + DateTime.Now);
+                this.Progress?.StartStepWithoutProgress("Total time: " + sw.Elapsed);
+            }
+        }
+    }
+}
