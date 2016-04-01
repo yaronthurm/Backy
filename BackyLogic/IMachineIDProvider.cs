abb1ffdb90c074482a675fb45aaf51e4b2f3aac4
yaronthurm <yaronthurm@gmail.com>
2016-04-01 09:58:08 +0300

    added MachineIDProvider

---
+++ BackyLogic/IMachineIDProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackyLogic
+{
+    public interface IMachineIDProvider
+    {
+        string GetOrCreateID();
+    }
+
+}
---
+++ BackyLogic/OSMachineIDProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BackyLogic
+{
+    public class OSMachineIDProvider : IMachineIDProvider
+    {
+        public string GetOrCreateID()
+        {
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            var filePath = Path.Combine(appDataPath, "Backy", "Backy.ini");
+            if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                var ret = Guid.NewGuid().ToString("N");
+                File.WriteAllText(filePath, ret);
+                return ret;
+            }
+            else
+            {
+                var ret = File.ReadAllText(filePath);
+                return ret;
+            }
+        }
+    }
+
+}
