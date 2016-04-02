86c7f99e06e59ba99c42a953f4823fa87241d349
yaronthurm <yaronthurm@gmail.com>
2016-04-02 23:36:24 +0300

    added checks to ok button on settings form

--- BackyUI/Settings.Designer.cs
+++ BackyUI/Settings.Designer.cs
@@ -31,10 +31,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnBrowseFoldersTarget = new System.Windows.Forms.Button();
-            this.backupTargetView1 = new Backy.BackupTargetView();
-            this.addSourcesPanel1 = new Backy.AddBackupSourcesPanel();
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.backupTargetView1 = new Backy.BackupTargetView();
+            this.addSourcesPanel1 = new Backy.AddBackupSourcesPanel();
             this.SuspendLayout();
             // 
             // label1
@@ -65,6 +65,26 @@
             this.btnBrowseFoldersTarget.UseVisualStyleBackColor = true;
             this.btnBrowseFoldersTarget.Click += new System.EventHandler(this.btnBrowseFoldersTarget_Click);
             // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(471, 452);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 23;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(562, 452);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 24;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
             // backupTargetView1
             // 
             this.backupTargetView1.BackColor = System.Drawing.SystemColors.Control;
@@ -83,26 +103,6 @@
             this.addSourcesPanel1.Name = "addSourcesPanel1";
             this.addSourcesPanel1.Size = new System.Drawing.Size(637, 243);
             this.addSourcesPanel1.TabIndex = 0;
-            // 
-            // btnOK
-            // 
-            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(471, 452);
-            this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
-            this.btnOK.TabIndex = 23;
-            this.btnOK.Text = "OK";
-            this.btnOK.UseVisualStyleBackColor = true;
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(562, 452);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
-            this.btnCancel.TabIndex = 24;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // Settings
             // 
--- BackyUI/Settings.cs
+++ BackyUI/Settings.cs
@@ -47,5 +47,22 @@ namespace Backy
             if (!string.IsNullOrEmpty(settings.Target))
                 this.backupTargetView1.SetDirectory(settings.Target);
         }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty( this.backupTargetView1.Path))
+            {
+                MessageBox.Show("Please select a target directory");
+                return;
+            }
+
+            if (!this.addSourcesPanel1.GetSelectedSources().Any())
+            {
+                MessageBox.Show("Please select at least one source directory");
+                return;
+            }
+
+            this.DialogResult = DialogResult.OK;
+        }
     }
 }
