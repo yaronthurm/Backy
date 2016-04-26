2506be14d9ba711ede50676ddf32c23d08188831
yaronthurm <yaronthurm@gmail.com>
2016-04-26 11:19:43 +0300

    added new failed test

--- TestBacky/TestBAckupTheBackupCommand.cs
+++ TestBacky/TestBAckupTheBackupCommand.cs
@@ -54,5 +54,66 @@ namespace TestBacky
             var actual = fs.EnumerateFiles(backupOfTheBackupTarget).Select(x => EmulatorFile.FromlFileName(x, x.Replace(backupOfTheBackupTarget + "\\", ""), fs)).ToArray();
             TestsUtils.AssertEmulatorFiles(fs, expected, actual, "");
         }
+
+        [TestMethod]
+        public void BackupTheBackup_02_Running_on_a_single_source_for_the_second_time()
+        {
+            // This test simulates backing up the backup when there is only one source the copy
+            // We will run the command once, do some additional backups and run it again.
+
+            var backupSource = @"c:\source";
+            var backupTarget = @"d:\target";
+            var backupOfTheBackupTarget = @"e:\target";
+
+            var files = new EmulatorFile[] {
+                new EmulatorFile(@"c:\source\file1"),
+                new EmulatorFile(@"c:\source\file2"),
+                new EmulatorFile(@"c:\source\file3"),
+                new EmulatorFile(@"c:\source\dir1\file1"),
+                new EmulatorFile(@"c:\source\dir1\file2"),
+                new EmulatorFile(@"c:\source\dir1\file3"),
+                new EmulatorFile(@"c:\source\dir1\dir1\file1"),
+                new EmulatorFile(@"c:\source\dir1\dir1\file2"),
+                new EmulatorFile(@"c:\source\dir1\dir2\file1"),
+                new EmulatorFile(@"c:\source\dir2\file1"),
+                new EmulatorFile(@"c:\source\dir2\file2") };
+
+            var fs = new FileSystemEmulator(files);
+            var cmd = new RunBackupCommand(fs, backupSource, backupTarget);
+            cmd.Execute();
+
+            fs.AddFiles(new[] {
+                new EmulatorFile(@"c:\source\dir3\file1"),
+                new EmulatorFile(@"c:\source\dir3\file2"),
+                new EmulatorFile(@"c:\source\dir3\file3") });
+
+            cmd.Execute();
+
+            // First time of backing up the backup
+            var btbCommand = new BackupTheBackupCommand(fs, backupTarget, backupOfTheBackupTarget);
+            btbCommand.Execute();
+
+            // Do some regular backups
+            fs.AddFiles(new[] {
+                new EmulatorFile(@"c:\source\dir4\file1"),
+                new EmulatorFile(@"c:\source\dir4\file2"),
+                new EmulatorFile(@"c:\source\dir4\file3") });
+            cmd.Execute();
+
+            fs.AddFiles(new[] {
+                new EmulatorFile(@"c:\source\dir5\file1"),
+                new EmulatorFile(@"c:\source\dir5\file2"),
+                new EmulatorFile(@"c:\source\dir5\file3") });
+            cmd.Execute();
+
+
+            // Run the backup of the backup again
+            btbCommand.Execute();
+
+
+            var expected = fs.EnumerateFiles(backupTarget).Select(x => EmulatorFile.FromlFileName(x, x.Replace(backupTarget + "\\", ""), fs)).ToArray();
+            var actual = fs.EnumerateFiles(backupOfTheBackupTarget).Select(x => EmulatorFile.FromlFileName(x, x.Replace(backupOfTheBackupTarget + "\\", ""), fs)).ToArray();
+            TestsUtils.AssertEmulatorFiles(fs, expected, actual, "");
+        }
     }
 }
