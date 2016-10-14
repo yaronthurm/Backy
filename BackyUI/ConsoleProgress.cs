7e401080607d4b5f6df48124504226a92d505ca6
yaronthurm <yaronthurm@gmail.com>
2016-10-14 12:49:33 +0300

    Added option to run as console app

---
+++ BackyUI/ConsoleProgress.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BackyLogic;
+
+namespace Backy
+{
+    internal class ConsoleProgress : IMultiStepProgress
+    {
+        private string _lastText;
+        private int _currentValue;
+        private int _maxValue;
+
+        public void Increment()
+        {
+            Console.Write($"\r{_lastText}".PadRight(Console.BufferWidth, ' '));
+            Console.Write($"\r{_lastText}: {++_currentValue}/{_maxValue}");
+        }
+
+        public void StartBoundedStep(string text, int maxValue)
+        {
+            _currentValue = 0;
+            _maxValue = maxValue;
+            _lastText = text;
+            Console.Write("\n" + _lastText);
+        }
+
+        public void StartStepWithoutProgress(string text)
+        {
+            Console.Write($"\n{text}");
+        }
+
+        public void StartUnboundedStep(string text, Func<int, string> projection = null)
+        {
+            _lastText = text;
+            Console.Write($"\n{text}");
+        }
+
+        public void UpdateProgress(int currentValue)
+        {
+            Console.Write($"\r{_lastText}".PadRight(Console.BufferWidth, ' '));
+            Console.Write($"\r{_lastText}: {currentValue}");
+        }
+    }
+}
--- BackyUI/Program.cs
+++ BackyUI/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BackyLogic;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -17,8 +19,14 @@ namespace Backy
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (ShouldOpenAsConsoloeApp(args))
+            {
+                RunAsConsoleApp();
+                return;
+            }
+
             if (singleInstanceMutex.WaitOne(TimeSpan.Zero, true))
             {
                 Application.EnableVisualStyles();
@@ -28,6 +36,44 @@ namespace Backy
             else {
                 FocusOnActiveInstance();
             }
+        }
+
+        private static void RunAsConsoleApp()
+        {
+            AllocConsole();
+            BackyLogic.Settings _settings = BackyLogic.Settings.Load();
+            IFileSystem fileSystem = new OSFileSystem();
+
+            if (!Directory.Exists(_settings.Target))
+            {
+                Console.WriteLine($"Target directory '{_settings.Target}' does not exist. Please select a different target.");
+                return;
+            }
+            var activeSources = _settings.Sources.Where(x => x.Enabled && Directory.Exists(x.Path)).ToArray();
+            
+
+            var _cancelTokenSource = new CancellationTokenSource();
+            var backupCommands = activeSources
+                .Select(x =>
+                new RunBackupCommand(fileSystem, x.Path, _settings.Target, new MachineID { Value = _settings.MachineID },
+                _cancelTokenSource.Token) {
+                    Progress = new ConsoleProgress() });
+
+            if (!backupCommands.Any()) {
+                new ConsoleProgress().StartStepWithoutProgress("There are no active sources");
+                return;
+            }
+
+            foreach (var cmd in backupCommands)
+            {
+                cmd.Execute();
+            }
+        }
+
+        private static bool ShouldOpenAsConsoloeApp(string[] args)
+        {
+            var ret = args != null && args.Length == 1 && args[0] == "-console";
+            return ret;
         }
 
         static void FocusOnActiveInstance()
@@ -46,5 +92,9 @@ namespace Backy
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
     }
 }
